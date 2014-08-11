bd8c61d859b14048d4914bcd273a6ce89f46f055
hellobrowncow <seventhavenue@hotmail.com>
2014-08-11 21:44:11 +0800

    Added completed completed games section in home.html and home.js

--- client/home.js
+++ client/home.js
@@ -3,6 +3,18 @@ function otherId(game) {
 } 
 
 Template.gameList.helpers({
+  completedGames: function () {
+    return Games.find({ inProgress: false }).map(function (game) {
+      game.otherPlayer = Meteor.users.findOne(otherId(game)).username;
+      game.finished = moment(game.finished).fromNow(); 
+    
+      if (game.winner === 'tie') game.message = 'tied';
+      else if (game.winner === Meteor.userId()) game.message = 'won';
+      else game.message = "lost";
+      return game;
+    });
+  },   
+
   games: function () {
     return Games.find({ inProgress: true }).map(function (game) {
       game.otherPlayer = Meteor.users.findOne(otherId(game)).username;
