78b7df937a1c790f54f4ad9d6406e1b75e292910
hellobrowncow <seventhavenue@hotmail.com>
2014-08-18 17:30:11 +0800

    Minor naming fix  gameCreation.js

--- gameCreation.js
+++ gameCreation.js
@@ -59,7 +59,7 @@ function createDeck () {
     for (var i = 1; i <= 13; i++) {
         var name = i;
         if (i === 1) name = 'A';
-        if (i === 11) name = 'N';
+        if (i === 11) name = 'J';
         if (i === 12) name = 'Q';
         if (i === 13) name = 'K';
         cards.push({
